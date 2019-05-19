f060dd7d6e827eb3051ecbc233515d1d5655378a
Jordan Osborn <jordanosborn0@gmail.com>
2019-05-19 19:42:38 +0100

    removed items from world

--- World.cs
+++ World.cs
@@ -10,12 +10,34 @@ namespace Potential
             IntrinsicFields = new List<Field>();
             Particles = new List<Particle>();
         }
-        public void AddField(Field f) {
-            IntrinsicFields.Add(f);
-        }
-        public void AddParticle(Field f)
+        public void AddField(Field f)
         {
             IntrinsicFields.Add(f);
+        }
+        public void AddParticle(Particle p)
+        {
+            Particles.Add(p);
+        }
+
+        public Particle RemoveParticle(uint id)
+        {
+            if (id < Particles.Count)
+            {
+                var particle = Particles[(int)id];
+                Particles.RemoveAt((int)id);
+                return particle;
+            }
+            return null;
+        }
+        public Field RemoveField(uint id)
+        {
+            if (id < IntrinsicFields.Count)
+            {
+                var field = IntrinsicFields[(int)id];
+                IntrinsicFields.RemoveAt((int)id);
+                return field;
+            }
+            return null;
         }
     }
 }
