a668d668961a8d17316c41047783759ee1205646
Jordan Osborn <jordanosborn0@gmail.com>
2019-05-20 15:00:40 +0100

    moved transient Particle

--- Particle.cs
+++ Particle.cs
@@ -2,17 +2,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 namespace Potential
 {
-    public class TransientParticle : Particle
-    {
-        private float Lifetime { get; } = float.PositiveInfinity;
-        private float Age { get; } = 0.0f;
-        //TODO: decay products
-        public TransientParticle(Texture2D texture, Vector3 position, Vector3 velocity, float radius = 10.0f, float mass = 0, float charge = 0, float energy = -1.0f, float rotation = 0.0f, float angular_velocity = 0.0f, bool isfixed = false, float lifetime = float.PositiveInfinity) :
-        base(texture, position, velocity, radius, mass, charge, energy, rotation, angular_velocity, isfixed)
-        {
-
-        }
-    }
     public class Particle : GameObject
     {
         public static float MAXSPEED = 10.0f;
