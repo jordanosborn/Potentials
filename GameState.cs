dc59b1940d0683cebc460bf3390c0181a5ab438b
Jordan Osborn <jordanosborn0@gmail.com>
2019-05-22 14:34:41 +0100

    added UI for parameters

--- GameState.cs
+++ GameState.cs
@@ -2,6 +2,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
+using System.Linq;
 
 using System;
 namespace Potential
@@ -9,6 +10,30 @@ namespace Potential
     using Utilities;
     public class GameState
     {
+        private class ParamValues
+        {
+            public float Mass { get; set; } = 0;
+            public float Charge { get; set; } = 0;
+            public float Radius { get; set; } = 0;
+            public Vector3 Position { get; set; } = Vector3.Zero;
+            public Vector3 Velocity { get; set; } = Vector3.Zero;
+            public void Update(float? mass, float? charge = null, float? radius = null, Vector3? position = null, Vector3? velocity = null)
+            {
+                Mass = mass ?? Mass;
+                Charge = charge ?? Charge;
+                Radius = radius ?? Radius;
+                Position = position ?? Position;
+                Velocity = velocity ?? Velocity;
+            }
+        }
+        public enum Param
+        {
+            MASS = 0,
+            CHARGE = 1,
+            RADIUS = 2,
+            POSITION = 3,
+            VELOCITY = 4,
+        }
         public enum UIFlags
         {
             TRACERS_ON,
@@ -16,6 +41,7 @@ namespace Potential
             FPS_ON,
             MOUSE_LOCATION_ON
         }
+        private static int ParameterCount = (Enum.GetValues(typeof(Param))).Length;
         public enum MouseButtonCode
         {
             LEFT,
@@ -26,8 +52,11 @@ namespace Potential
         private Color ColorBG = Color.Black;
         private SpriteFont Font { get; set; } = null;
         readonly SmartFramerate FPS = new SmartFramerate(5, (4, 0));
+        readonly Vector2 SelectedPosition = new Vector2(4, 20);
         private Keys? LastPressed { get; set; } = null;
         private MouseButtonCode? LastClicked { get; set; } = null;
+        private Param ParamSelected { get; set; } = Param.MASS;
+        private ParamValues paramValues { get; set; } = new ParamValues();
         public HashSet<UIFlags> Flags { get; private set; } = new HashSet<UIFlags>();
         public bool IsPaused { get => Flags.Contains(UIFlags.PAUSED); }
         public static GameState state;
@@ -53,6 +82,42 @@ namespace Potential
             Flags.Add(UIFlags.FPS_ON);
             Flags.Add(UIFlags.MOUSE_LOCATION_ON);
             Flags.Add(UIFlags.TRACERS_ON);
+        }
+
+        private void DrawParamUI(SpriteBatch batch, Vector2 location)
+        {
+            var TextUI = new List<string>();
+            TextUI.Add($"Mass: {paramValues.Mass}");
+            TextUI.Add($"Charge: {paramValues.Charge}");
+            TextUI.Add($"Radius: {paramValues.Radius}");
+            TextUI.Add($"Position: {paramValues.Position}");
+            TextUI.Add($"Velocity: {paramValues.Velocity}");
+            string filterText;
+            switch (ParamSelected)
+            {
+                case Param.MASS:
+                    filterText = "Mass";
+                    break;
+                case Param.CHARGE:
+                    filterText = "Charge";
+                    break;
+                case Param.RADIUS:
+                    filterText = "Radius";
+                    break;
+                case Param.POSITION:
+                    filterText = "Position";
+                    break;
+                case Param.VELOCITY:
+                    filterText = "Velocity";
+                    break;
+                default:
+                    filterText = "";
+                    break;
+            }
+            string selectedText = string.Join("    ", TextUI.Where((s) => s.Contains(filterText)).ToList());
+            string unselectedText = string.Join("    ", TextUI.Where((s) => !s.Contains(filterText)).ToList());
+            batch.DrawString(Font, unselectedText, location, ColorFG);
+            batch.DrawString(Font, selectedText, SelectedPosition, Color.Green);
         }
 
         public ErrorCodes Draw(SpriteBatch batch, GameWindow window = null)
@@ -84,6 +149,7 @@ namespace Potential
             }
             var s = string.Join("    ", TextUI);
             batch.DrawString(Font, s, FPS.Position, ColorFG);
+            DrawParamUI(batch, new Vector2(4, window.ClientBounds.Height - 20));
             return ErrorCodes.SUCCESS;
         }
         private ErrorCodes MouseClick(MouseState pressed, MouseButtonCode button, Func<ErrorCodes> func)
@@ -140,9 +206,14 @@ namespace Potential
                 }
                 return ErrorCodes.SUCCESS;
             });
+            KeyPress(keyboardState, Keys.Tab, () =>
+            {
+                ParamSelected = (Param)(((int)ParamSelected + 1) % (ParameterCount));
+                return ErrorCodes.SUCCESS;
+            });
             MouseClick(mouseState, MouseButtonCode.LEFT, () =>
             {
-                Console.WriteLine($"{mouseState.X}, {mouseState.Y}");
+                paramValues.Position = new Vector3(mouseState.X, mouseState.Y, 0);
                 return ErrorCodes.SUCCESS;
             });
 
