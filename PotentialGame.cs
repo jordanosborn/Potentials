cac46b222ee2c8fbbb9c1afcc7b977e633e4e1ab
Jordan Osborn <jordanosborn0@gmail.com>
2019-05-23 11:10:09 +0100

    added arrow icon

--- PotentialGame.cs
+++ PotentialGame.cs
@@ -1,34 +1,26 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
+using Potential.Force;
 
 namespace Potential
 {
     public class PotentialGame : Game
     {
-        private Dictionary<string, Texture2D> Textures = new Dictionary<string, Texture2D>();
-        private (int, int) DefaultSize = (600, 600);
-        private (int, int) MinSize = (600, 600);
-        private SpriteFont Font = null;
+        private readonly Color ColorBG = Color.Black;
+        private readonly (int, int) DefaultSize = (600, 600);
+        private readonly GraphicsDeviceManager graphics;
+        private readonly (int, int) MinSize = (600, 600);
+        private readonly Dictionary<string, Texture2D> Textures = new Dictionary<string, Texture2D>();
         private Color ColorFG = Color.White;
-        private Color ColorBG = Color.Black;
-        private World GameWorld = null;
-        private MouseCursor mouseCursor;
-        GameState State;
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
-        private void HandleResizeEvent(object sender, EventArgs a)
-        {
-            if (Window.ClientBounds.Height < MinSize.Item2)
-                graphics.PreferredBackBufferHeight = MinSize.Item2;
-            if (Window.ClientBounds.Width < MinSize.Item1)
-                graphics.PreferredBackBufferWidth = MinSize.Item1;
-            graphics.ApplyChanges();
-        }
+        private MouseCursor Cursor;
+        private SpriteFont Font;
+        private World GameWorld;
+        private SpriteBatch spriteBatch;
+        private GameState State;
 
         public PotentialGame()
         {
@@ -43,22 +35,33 @@ namespace Potential
             Window.ClientSizeChanged += HandleResizeEvent;
         }
 
+        private void HandleResizeEvent(object sender, EventArgs a)
+        {
+            if (Window.ClientBounds.Height < MinSize.Item2)
+                graphics.PreferredBackBufferHeight = MinSize.Item2;
+            if (Window.ClientBounds.Width < MinSize.Item1)
+                graphics.PreferredBackBufferWidth = MinSize.Item1;
+            graphics.ApplyChanges();
+        }
+
         protected override void Initialize()
         {
             LoadContent();
             GameWorld = new World(Textures);
-            mouseCursor = MouseCursor.FromTexture2D(Textures["cursor"],
+            Cursor = MouseCursor.FromTexture2D(Textures["cursor"],
                 Textures["cursor"].Width / 2, Textures["cursor"].Height / 2);
-            Mouse.SetCursor(mouseCursor);
+            Mouse.SetCursor(Cursor);
             GameWorld.AddParticle(new Particle(Textures["blackhole"], new Vector3(400, 400, 0),
-                new Vector3(0, -50, 0), 50, mass: 0, angular_velocity: -0.8f, isfixed: false));
-            GameWorld.Particles[0].ParticleTracer = new Tracer(Textures["tracer"], GameWorld.Particles[0], color: Color.Red);
+                new Vector3(0, -50, 0), 50, 0, angular_velocity: -0.8f));
+            GameWorld.Particles[0].ParticleTracer =
+                new Tracer(Textures["tracer"], GameWorld.Particles[0], color: Color.Red);
             GameWorld.AddParticle(new Particle(Textures["moon"], new Vector3(400, 400, 0),
-                new Vector3(0, 50, 0), 50, mass: 0));
-            GameWorld.Particles[1].ParticleTracer = new Tracer(Textures["tracer"], GameWorld.Particles[1], color: Color.Blue);
+                new Vector3(0, 50, 0), 50));
+            GameWorld.Particles[1].ParticleTracer =
+                new Tracer(Textures["tracer"], GameWorld.Particles[1], color: Color.Blue);
             GameWorld.Particles[1].AddInterParticleForceSymmetric(
                 GameWorld.Particles[0],
-                Force.Factory.SpringForce(0.001f, 0.0f)
+                Factory.SpringForce(0.001f, 0.0f)
             );
             base.Initialize();
         }
@@ -70,6 +73,7 @@ namespace Potential
             Textures["blackhole"] = Content.Load<Texture2D>("blackhole");
             Textures["cursor"] = Content.Load<Texture2D>("cursor");
             Textures["tracer"] = Content.Load<Texture2D>("circle");
+            Textures["arrow"] = Content.Load<Texture2D>("arrow");
             Textures["background"] = Content.Load<Texture2D>("background");
             Font = Content.Load<SpriteFont>("Font");
             State = GameState.GetState(Font);
@@ -77,17 +81,14 @@ namespace Potential
 
         protected override void Update(GameTime gameTime)
         {
-            ButtonState[] MouseButtonStates = { Mouse.GetState().LeftButton, Mouse.GetState().MiddleButton, Mouse.GetState().RightButton };
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            ButtonState[] MouseButtonStates =
+                {Mouse.GetState().LeftButton, Mouse.GetState().MiddleButton, Mouse.GetState().RightButton};
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            if (MouseButtonStates.Any((b) => b == ButtonState.Pressed))
-            {
+            if (MouseButtonStates.Any(b => b == ButtonState.Pressed))
                 Mouse.SetCursor(MouseCursor.Crosshair);
-            }
-            else if (MouseButtonStates.Any((b) => b == ButtonState.Released))
-            {
-                Mouse.SetCursor(mouseCursor);
-            }
+            else if (MouseButtonStates.Any(b => b == ButtonState.Released)) Mouse.SetCursor(Cursor);
             if (!State.IsPaused)
             {
                 //TODO: should this be cloned here?
@@ -97,7 +98,8 @@ namespace Potential
                 GameWorld.Update(gameTime);
                 base.Update(gameTime);
             }
-            State.Update(Keyboard.GetState(), Mouse.GetState(), ref GameWorld, gameTime, null);
+
+            State.Update(Keyboard.GetState(), Mouse.GetState(), ref GameWorld, gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
@@ -105,9 +107,10 @@ namespace Potential
             GraphicsDevice.Clear(ColorBG);
             spriteBatch.Begin();
             var bg = Textures["background"];
-            spriteBatch.Draw(bg, position: Vector2.Zero,
+            spriteBatch.Draw(bg, Vector2.Zero,
                 origin: Vector2.Zero, color: Color.White,
-                scale: new Vector2(Window.ClientBounds.Width / (float)bg.Width, Window.ClientBounds.Height / (float)bg.Height)
+                scale: new Vector2(Window.ClientBounds.Width / (float) bg.Width,
+                    Window.ClientBounds.Height / (float) bg.Height)
             );
             GameWorld.Draw(spriteBatch);
             State.Draw(spriteBatch, GameWorld, Window);
