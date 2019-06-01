4c4663c1dffba50ef7850cba3a422b8b8e464b35
Jordan Osborn <jordanosborn0@gmail.com>
2019-06-01 22:19:21 +0100

    added test suite

--- IGameObject.cs
+++ IGameObject.cs
@@ -10,8 +10,6 @@ namespace Potential
         ErrorCodes Update(GameTime gameTime, World world = null, GameState state = null, object args = null);
         ErrorCodes Draw(SpriteBatch batch);
         //Returns shape of objet true if vector inside shape.
-        bool Shape(Vector3 pos);
-        //TODO: if two shapes collide, null if no collision, returns strength np.inf (totally elastic, instant vel change)
-        float? CollisionCheck(IGameComponent collider);
+        // w
     }
 }
