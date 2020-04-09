578cade821598a0abe7e910ac9a1bc756fca990f
Jordan Osborn <jordan@osborn.dev>
2020-04-09 14:25:45 +0100

    Modified test

--- Tests/Add.cs
+++ Tests/Add.cs
@@ -7,13 +7,14 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
-            
+
         }
 
-        [TestCase(8, 9, 20)]
-        public void Add(int x, int y, int z)
+        [TestCase(8, 9, 20, 37)]
+        [TestCase(7, 2, 5, 14)]
+        public void Add(int x, int y, int z, int answer)
         {
-            Assert.AreEqual(x + y + z, 37);
+            Assert.AreEqual(x + y + z, answer);
         }
     }
 }
